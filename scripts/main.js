eb344c701bcbd9243fa4c03edffc0ed9ea3d1e67
aliciaparau25 <pollitoconpapaylechugabr>
2026-04-16 17:55:58 +0200

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,37 @@
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "¡Hola mundo!";
+let myImage = document.querySelector("img");
+let myButton = document.querySelector("button");
+
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "Imagenes/Mozilla.png") {
+    myImage.setAttribute("src", "Imagenes/Mozilla2.png");
+  } else {
+    myImage.setAttribute("src", "Imagenes/Mozilla.png");
+  }
+};
+
+
+function setUserName() {
+  let myName = prompt("Por favor, introduce tu nombre.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla es genial, " + myName;
+  }
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla es genial, " + storedName;
+}
+
+
+myButton.onclick = function() {
+  setUserName();
+};
